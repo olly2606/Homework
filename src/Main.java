6c4b5adfe5d2e57c1398ec2cadf2af5c1eaa2e0d
Brakhotskaya Olga <brakhotskaya.oll@gmail.com>
2022-05-28 22:25:01 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        float amount  = 100.00f;
-        float cash  = 1100.00f;
+        float amount = 100.00f;
+        float cash = 1100.00f;
         float bonus = cash * 0.01f;
         float refill = amount + cash;
-        if (refill >=1000) {
+        if (refill >= 1000) {
             refill = refill + bonus;
         } else {
-            refill=refill;
+            refill = refill;
         }
         System.out.println(refill);
     }
